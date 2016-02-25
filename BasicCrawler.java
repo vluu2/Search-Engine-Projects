abe7ba8cac5565849837217b6621aaf6c465c8b9
ligerx123 <ligerx066@gmail.com>
2016-02-25 00:08:43 -0800

    finding unique words partially working

--- BasicCrawler.java
+++ BasicCrawler.java
@@ -21,6 +21,7 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
@@ -32,7 +33,7 @@ import com.mongodb.util.JSON;
 /* 
  * We used http://www.mkyong.com/java/jsoup-html-parser-hello-world-examples/ as reference for our extractor 
  * We used http://jsoup.org/cookbook/extracting-data/example-list-links to get the http links
- * We used http://www.mkyong.com/java/how-do-convert-byte-array-to-string-in-java/ for hashing
+ * We used http://www.mkyong.com/java/java-sha-hashing-example/
  * 
  * 
  * 
@@ -44,7 +45,12 @@ public class BasicCrawler
 	{			 
 		MongoClient mongo = new MongoClient( "localhost" , 27017 );
 		DB db = mongo.getDB("cs454-db");
-		DBCollection collection = db.getCollection("dummyTable");	
+		
+		DBCollection collection = db.createCollection("dummyTable", new BasicDBObject());	//.getCollection("dummyTable");	
+		
+//		DB worddb = mongo.getDB("wordDB");
+		DBCollection word = db.createCollection("UniqueWords", new BasicDBObject());	
+		
 		
 		int count = 1;
 		String original = args[1];
@@ -53,10 +59,7 @@ public class BasicCrawler
 		String depth = args[2];
 		String lvl = args[3];
 				
-		boolean ex = false;
-		
-		
-		
+		boolean ex = false;	
 		
 		if( !(url.equals("-u")) || (!(depth.equals("-d"))) )  
 		{
@@ -70,7 +73,7 @@ public class BasicCrawler
 		
 		if(args.length == 4)
 		{
-			crawlUrl(collection, cUrl, args[3], count, avu, ex);
+			crawlUrl(word, collection, cUrl, args[3], count, avu, ex);
 		}
 		else if(args.length == 5)
 		{
@@ -79,7 +82,7 @@ public class BasicCrawler
 			if( extract.equals("-e") )
 			{
 				ex = true;			
-				crawlUrl(collection, cUrl, args[3], count, avu, ex);
+				crawlUrl(word, collection, cUrl, args[3], count, avu, ex);
 			}
 			else
 			{
@@ -92,8 +95,8 @@ public class BasicCrawler
 			System.exit(0);
 		}		
 	}
- //-------------------------------------------------------------------------------------------
-	static void crawlUrl(DBCollection collection, ArrayList<String> currentList, String depth, int count, 
+//-------------------------------------------------------------------------------------------
+	static void crawlUrl(DBCollection word, DBCollection collection, ArrayList<String> currentList, String depth, int count, 
 			ArrayList<String> p, boolean x)	
 	{
 		int tracker = count;
@@ -126,8 +129,10 @@ public class BasicCrawler
 							tester.put("Title", doc.title());	//gets the name of official website
 							tester.put("URL", currentList.get(i));
 								
-							String text = doc.select("body").text();
+							String text = doc.select("body").text();							
 							tester.put("Text", text);
+							
+							checkWord(word, text);
 							
 							System.out.println("All Text on site : " + text);
 														
@@ -201,7 +206,7 @@ public class BasicCrawler
 			System.out.println("Newdep contains: " + newDep);
 			System.out.println("Tracker contains: " + tracker);
 			
-			crawlUrl(collection, list , newDep, tracker, p, x);
+			crawlUrl(word, collection, list , newDep, tracker, p, x);
 		}
 		else
 		{
@@ -237,4 +242,138 @@ public class BasicCrawler
 		
 		return hashName;
 	}
+//-----------------------------------------------------------------------------------------------------------------
+	public static void checkWord(DBCollection word, String texts)
+	{
+		String[] array = texts.split(" ");
+		
+		String[] check = {"a", "able", "about", "above", "abst", "accordance", "accordingly", "across", "act", 
+							"actually", "added", "adj", "affected", "affecting", "affects", "after", "afterwards", "again", "against", 
+							"ah", "all", "almost", "alone", "along", "already", "also", "although", "always", "am", "among", "amongst",
+							"an", "and", "announce", "another", "any", "anybody", "anyhow", "anymore", "anyone", "anything", "anyway", "anyways",
+							"anywhere", "apparently", "approximately", "aren", "arent", "aren't", "arise", "around", "as", "aside", "ask", 
+							"asking", "at", "auth", "available", "away", "awfully", "back", "be", "became", "because", "become", "becomes", 
+							"becoming", "been", "before", "beforehand", "begin", "beginning", "beginnings", "begins", "behind", "being", "believe",
+							"below", "beside", "besides", "between", "beyond", "biol", "both", "brief", "briefly", "but", "by", "came", "can",
+							"cannot", "can't", "cause", "causes", "certain", "certainly", "come", "comes", "contain", "containing", "contains", 
+							"could", "couldn't", "date", "did", "didn't", "different", "do", "does", "doesn't", "doing", "done", "don't", "down",
+							"downwards", "due", "during", "each", "ed", "edu", "effect", "eg", "eight", "eighty", "either", "else", "elsewhere", 
+							"end", "ending", "enough", "especially", "et", "et-al", "etc", "even", "ever", "every", "everybody", "everyone", 
+							"everything", "everywhere", "ex", "except", "far", "few", "fifth", "first", "five", "fix", "followed", "following",
+							"follows", "for", "former", "formerly", "forth", "found", "four", "from", "further", "furthermore", "gave", "get", 
+							"gets", "getting", "give", "given", "gives", "giving", "go", "goes", "gone", "got", "gotten", "had", "happens", 
+							"hardly", "has", "hasn't", "have", "haven't", "having", "he", "hed", "hence", "her", "here", "hereafter", "hereby", 
+							"herein", "heres", "hereupon", "hers", "herself", "hes", "hi", "hid", "him", "himself", "his", "hither", "home", "how", 
+							"howbeit", "however", "hundred", "i", "id", "ie", "if", "i'll", "im", "immediate", "immediately", "importance", 
+							"important", "in", "inc", "indeed", "index", "information", "instead", "into", "invention", "inward", "is", "isn't", 
+							"it", "it'd", "it'll", "its", "itself", "i've", "just", "keep", "keeps", "kept", "kg", "km", "know", "known", 
+							"largely", "last", "lately", "later", "latter", "latterly", "least", "less", "lest", "let", "lets", "like", "liked",
+							"likely", "line", "little", "'ll", "look", "looking", "looks", "ltd", "made", "mainly", "make", "makes", "many", 
+							"may", "maybe", "me", "mean", "means", "meantime", "meanwhile", "merely", "mg", "might", "million", "miss", "ml", 
+							"more", "moreover", "most", "mostly", "mr", "mrs", "much", "mug", "must", "my", "myself", "name", "namely", "nay", 
+							"near", "nearly", "necessarily", "necessary", "need", "needs", "neither", "never", "nevertheless", "new", "next", 
+							"nine", "ninety", "no", "nobody", "non", "none", "nonetheless", "noone", "nor", "normally", "nos", "not", "noted", 
+							"nothing", "now", "nowhere", "obtain", "obtained", "obviously", "of", "off", "often", "oh", "ok", "okay", "old", 
+							"omitted", "on", "once", "one", "ones", "only", "onto", "or", "ord", "other", "others", "otherwise", "ought", "our",
+							"ourselves", "out", "outside", "over", "overall", "owing", "own", "page", "pages", "part", "particular", "particularly",
+							"past", "per", "perhaps", "placed", "please", "plus", "poorly", "possible", "possibly", "potentially", "pp", 
+							"predominantly", "present", "previously", "primarily", "probably", "promptly", "proud", "provides", "put", "que", 
+							"quickly", "quite", "ran", "rather", "re", "readily", "really", "recent", "recently", "ref", "refs", "regarding", 
+							"regardless", "regards", "related", "relatively", "research", "respectively", "resulted", "resulting", "results", 
+							"right", "run", "said", "same", "saw", "say", "saying", "says", "sec", "section", "see", "seeing", "seem", "seemed", 
+							"seeming", "seems", "seen", "self", "selves", "sent", "seven", "several", "shall", "she", "shed", "she'll", "shes", 
+							"should", "shouldn't", "show", "showed", "shown", "showns", "shows", "significant", "significantly", "similar", 
+							"simiarly", "since", "six", "slightly", "so", "some", "somebody", "somehow", "someone", "somethan", "something", 
+							"sometime", "sometimes", "somewhat", "somewhere", "soon", "sorry", "specifically", "specified", "specify", "specifying",
+							"still", "stop", "strongly", "sub", "substantially", "successfully", "such", "sufficiently", "suggest", "sup", "sure",
+							"take", "taken", "taken", "taking", "tell", "tends", "than", "thank", "thanks", "thanx", "that", "that'll", "thats", 
+							"that've", "the", "their", "theirs", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "thered", 
+							"therefore", "therein", "there'll", "thereof", "thereto", "thereupon", "there've", "these", "they", "they'll", "they're",
+							"they've", "think", "this", "those", "thou", "though", "thousand", "through", "throughout", "thru", "thus", "til", "tip",
+							"to", "together", "too", "took", "toward", "towards", "tried", "tries", "truly", "try", "trying", "twice", "two", "u", 
+							"under", "unfortunately", "unless", "unlike", "unlikely", "until", "unto", "up", "upon", "ups", "us", "use", "used", 
+							"useful", "usefully", "usefulness", "uses", "using", "usually", "value", "various", "'ve", "very", "via", "viz", "vol", 
+							"vols", "want", "wants", "was", "wasn't", "way", "we", "we'd", "welcome", "we'll", "went", "were", "weren't", "we've", 
+							"what", "whatever", "what'll", "what's", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", 
+							"wherein", "wheres", "whereupon", "wherever", "whether", "which", "while", "whim", "whither", "who", "who'd", "whoever",
+							"whole", "who'll", "whom", "whomever", "whos", "whose", "why", "widely", "willing", "wish", "with", "within", "without",
+							"won't", "words", "world", "would", "wouldn't", "www", "y", "yes", "yet", "you", "you'd", "you'll", "your", "you're", 
+							"yours", "yourself", "yourselves", "you've", "zero"
+						};
+		
+		int count = 0;
+		
+		for(int i = 0; i < array.length; i++)
+		{
+			for(int j = 0; j < check.length; j++)
+			{
+				if( !(array[i].equals(check[j])) )
+				{
+					count++;
+				}
+			}
+			
+			if( count == check.length )
+			{
+				addWord(word, array[i]);			
+			}
+			
+			count = 0;
+		}
+		
+		
+	}
+//-----------------------------------------------------------------------------------------------------------------
+	public static void addWord(DBCollection word, String uniqueWord)
+	{
+		JSONObject uWord = new JSONObject();
+		DBCursor cursor = word.getCollection("UniqueWords").find();
+		DBObject dbObject;
+		
+		if(word.count() == 0)
+		{
+			try 
+			{
+				uWord.put("Word", uniqueWord);
+				dbObject = (DBObject) JSON.parse(uWord.toString());
+				word.insert(dbObject);
+			} 
+			catch (JSONException e) 
+			{
+				e.printStackTrace();
+			}
+		}
+		else
+		{
+			BasicDBObject wjo;
+			int count = 0;
+			
+			while(cursor.hasNext())
+			{
+				wjo = (BasicDBObject) cursor.next();
+				
+				if( !(uniqueWord.equals(wjo.getString("Word"))) )
+				{
+					count++;
+				}
+				
+			}
+			
+			if( count == word.count())
+			{
+				try 
+				{
+					uWord.put("Word", uniqueWord);
+					dbObject = (DBObject) JSON.parse(uWord.toString());
+					word.insert(dbObject);
+				} 
+				catch (JSONException e) 
+				{
+					e.printStackTrace();
+				}
+			}
+			
+		}
+	}
+//-----------------------------------------------------------------------------------------------------------------
 }
